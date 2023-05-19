7865f4dca5f048aa906df2e84bf835c5be5e09a4
WJX2001 <1758807220@qq.com>
2023-05-19 16:02:43 +0800

    feat: 登录操作

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,27 +1,65 @@
 import { Card, Form, Input, Button, Checkbox } from 'antd'
 import logo from '@/assets/logo.png'
 import './index.scss'
+import { useStore } from '@/store'
 
 const Login = () => {
+  const {loginStore} = useStore()
+  const onFinish = (values) => {
+    console.log('Success:', values)
+    //TODO: 登录
+    loginStore.getToken({
+      mobile: values.username,
+      code: values.password
+    })
+  }
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form
-             initialValues={{ remember: true }}
+        {/* 子项用到的触发事件 */}
+        <Form 
+           onFinish={ onFinish }
+           initialValues={{
+             mobile: '13911111111',
+             code: '246810',
+             remember: true
+           }}
+
         >
-          <Form.Item>
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号码格式不对',
+                validateTrigger: 'onBlur',
+              },
+              { required: true, message: '请输入手机号' },
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
+          <Form.Item
+            name="code"
+            rules={[
+              { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur' },
+              { required: true, message: '请输入验证码' },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
           </Form.Item>
-          <Form.Item name="rememeber">
+          <Form.Item name="remember" valuePropName="checked">
             <Checkbox className="login-checkbox-label">
               我已阅读并同意「用户协议」和「隐私条款」
             </Checkbox>
           </Form.Item>
+
           <Form.Item>
             <Button type="primary" htmlType="submit" size="large" block>
               登录
